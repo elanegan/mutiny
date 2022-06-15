688e77bf568201aea59d609b7dd175f4ba69f315
elanegan <e.lanegan@lancaster.ac.uk>
2022-06-15 16:44:58 +0100

    Test sprite display

--- mutiny.c
+++ mutiny.c
@@ -4,16 +4,19 @@
 #include <panel.h>
 
 #define BUFFER_SIZE 32
+
+#define COLOR_BROWN 8
 #define BACKGROUND_PAIR 0
 #define MAP_BORDER_PAIR 1
 #define SEA_PAIR 2
 #define INFO_PAIR 3
 #define WHEEL_PAIR 4
+#define SHIP_PAIR 5
 
 WINDOW* createNewWindow(int height, int width, int initY, int initX, int colPair);
 void destroyWindow(WINDOW* localWin);
 void setupColors();
-void displaySprite(WINDOW* localWin, int yPos, int xPos, char* fileName);
+void displaySprite(WINDOW* localWin, int yPos, int xPos, char* fileName, int colPair);
 
 int main(int argc, char* argv[]) {
     WINDOW* mainWindow;
@@ -41,29 +44,28 @@ int main(int argc, char* argv[]) {
     for (int i = 0; i < 3; i++) {
         panels[i] = new_panel(windows[i]);
     }
-/*
-    %--.../    ,
-    \    /`-. /
-     \  /@   /`.
-      `/    @   )
-      ' `-./   ,'
-          / ```
-*/
-    /* mvwprintw(windows[0], 10, boatx, "%%--.../    ,");
-    mvwprintw(windows[0], 11, boatx, "\\    /`-. /");
-    mvwprintw(windows[0], 12, boatx, " \\  /@   /`.");
-    mvwprintw(windows[0], 13, boatx, "  `/    @   )");
-    mvwprintw(windows[0], 14, boatx, "  ' `-./   ,'");
-    mvwprintw(windows[0], 15, boatx, "      / ```"); */
-
-    displaySprite(windows[0], 10, 75, "./assets/ship");
 
     // updates
     update_panels();
     doupdate();
 
-    while (ch != KEY_F(1))
+    // display changing ship sprite
+    int shipNum = 0;
+    char shipNames[15];
+    while (ch != KEY_F(1)) {
         ch = getch();
+        switch(ch) {
+            case(KEY_UP):
+                snprintf(shipNames, 15, "./assets/ship%d", shipNum);
+                displaySprite(windows[0], 10, 75, shipNames, SHIP_PAIR);
+                shipNum++;
+                if (shipNum > 7)
+                    shipNum = 0;
+                // updates
+                update_panels();
+                doupdate();
+        }
+    }
     endwin();
 
     return 0;
@@ -93,14 +95,17 @@ void destroyWindow(WINDOW* localWin) {
 void setupColors() {
     start_color();
 
+    init_color(COLOR_BROWN, 500, 160, 0);
+
     init_pair(MAP_BORDER_PAIR,COLOR_YELLOW, COLOR_BLACK);
     init_pair(SEA_PAIR, COLOR_WHITE, COLOR_BLUE);
     init_pair(INFO_PAIR, COLOR_BLUE, COLOR_BLACK);
     init_pair(WHEEL_PAIR, COLOR_YELLOW, COLOR_BLACK);
+    init_pair(SHIP_PAIR, COLOR_BROWN, COLOR_BLUE);
 }
 
-void displaySprite(WINDOW* localWin, int yPos, int xPos, char* fileName) {
-    FILE* fileHandle = fopen("./assets/ship", "r");
+void displaySprite(WINDOW* localWin, int yPos, int xPos, char* fileName, int colPair) {
+    FILE* fileHandle = fopen(fileName, "r");
     if (fileHandle == NULL) {
         printw("Unable to open file.\n");
         return;
@@ -113,7 +118,9 @@ void displaySprite(WINDOW* localWin, int yPos, int xPos, char* fileName) {
     result = fgets(line, BUFFER_SIZE, fileHandle);
     line[strcspn(line, "\n")] = 0;
     while (result != NULL) {
+        wattron(localWin, COLOR_PAIR(colPair));
         mvwprintw(localWin, yPosition, xPos, "%s", result);
+        wattroff(localWin, COLOR_PAIR(colPair));
         yPosition++;
         result = fgets(line, BUFFER_SIZE, fileHandle);
         line[strcspn(line, "\n")] = 0;
