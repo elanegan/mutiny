bacd0e3a2573a9d7dbd4457a88d97701c9a38f02
elanegan <e.lanegan@lancaster.ac.uk>
2022-08-04 00:34:44 +0100

    Add documentation for wind.h

--- wind.h
+++ wind.h
@@ -1,15 +1,21 @@
+/// @file wind.h
 #ifndef WIND
 #include <math.h>
 #include <time.h>
 #define WIND
 
-    #define SPEED 1.5
+    #define SPEED 1.5 ///< Wind speed typical maximum
 
+    /// Data type for wind
     typedef struct Wind {
         double direction; // wind direction in radians (0-2pi)
         double speed; // absolute speed of wind
     } Wind;
 
+    /**
+     * Function to create new wind.
+     * @param[out] wind A pointer to a new wind type.
+     */
     Wind* createNewWind() {
         Wind* wind = malloc(sizeof(Wind));
         wind->direction = 0;
@@ -19,18 +25,36 @@
         return(wind);
     }
 
+    /**
+     * Destroys a wind object.
+     * @param[in] localWind The wind to destroy.
+     */
     void destroyWind(Wind* localWind) {
         free(localWind);
     }
 
+    /**
+     * Gets the direction of the wind.
+     * @param[in] localWind The wind to act on.
+     * @param[out] direction The direction of the wind in radians.
+     */
     double getWindDirection(Wind* localWind) {
         return(localWind->direction);
     }
 
+    /**
+     * Gets the absolute speed of the wind.
+     * @param[in] localWind The wind to act on.
+     * @param[out] speed The absolute speed of the wind.
+     */
     double getWindSpeed(Wind* localWind) {
         return(localWind->speed);
     }
 
+    /**
+     * Update function to vary direction and speed of wind.
+     * @param[in] localWind The wind to act on.
+     */
     void updateWind(Wind* localWind) {
         double currentSpeed = getWindSpeed(localWind);
         double currentDirection = getWindDirection(localWind);
