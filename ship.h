fb317c1a564646d1f6acfdae9dd304ee4e5585a8
elanegan <e.lanegan@lancaster.ac.uk>
2022-08-03 22:26:45 +0100

    Add documentation for ship.h

--- ship.h
+++ ship.h
@@ -1,3 +1,4 @@
+/// @file ship.h
 #ifndef SHIP
 #include <math.h>
 #include <string.h>
@@ -13,6 +14,7 @@
 #define SHIP_EAST 6
 #define SHIP_NORTH_EAST 7
 
+    /// Data type for a ship
     typedef struct Ship {
         int id; // unique identifier. player is 0.
         double health; //health, from 0-100.
@@ -29,6 +31,15 @@
         char spritePath[64]; // the path to the set of sprites to be used
     } Ship;
 
+    /** 
+     * Function to create a new ship.
+     * @param[in] identifier A unique identifier for the ship.
+     * @param[in] mapY The initial Y position for the ship on the map.
+     * @param[in] mapX The initial X position for the ship on the map.
+     * @param[in] screenY The initial Y position for the ship on the camera window.
+     * @param[in] screenX The initial X position for the ship on the camera window.
+     * @param[out] ship A pointer to the new ship.
+     */
     Ship* createNewShip(int identifier, double mapY, double mapX, int screenY, int screenX) {
         Ship* ship = malloc(sizeof(Ship));
         ship->id = identifier;
@@ -48,50 +59,110 @@
         return(ship);
     }
 
+    /**
+     * Destroys a ship object.
+     * @param[in] localShip The ship to be destroyed.
+     */
     void destroyShip(Ship* localShip) {
         free(localShip);
     }
 
+    /**
+     * Sets the health of a ship.
+     * @param[in] localShip The ship to act on.
+     * @param[in] newHealth The new health level to set for the ship.
+     */
     void setHealth(Ship* localShip, double newHealth) {
         localShip->health = newHealth;
     }
 
+    /**
+     * Gets the health of a ship.
+     * @param[in] localShip The ship to act on.
+     * @param[out] health The health level of the ship.
+     */
     double getHealth(Ship* localShip) {
         return(localShip->health);
     }
 
+    /**
+     * Sets a ship's X position on the map.
+     * @param[in] localShip The ship to act on.
+     * @param[in] newXPosition The new X position to set for the ship.
+     */
     void setXPosition(Ship* localShip, double newXPosition) {
         localShip->xPosition = newXPosition;
     }
 
+    /**
+     * Gets a ship's X position on the map.
+     * @param[in] localShip The ship to act on.
+     * @param[out] xPosition The X position of the ship.
+     */
     double getXPosition(Ship* localShip) {
         return(localShip->xPosition);
     }
 
+    /**
+     * Sets a ship's Y position on the map.
+     * @param[in] localShip The ship to act on.
+     * @param[in] newYPosition The new Y position to set for the ship.
+     */
     void setYPosition(Ship* localShip, double newYPosition) {
         localShip->yPosition = newYPosition;
     }
 
+    /**
+     * Gets a ship's Y position on the map.
+     * @param[in] localShip The ship to act on.
+     * @param[out] yPosition The Y position of the ship.
+     */
     double getYPosition(Ship* localShip) {
         return(localShip->yPosition);
     }
 
+    /**
+     * Sets a ship's X position on the camera window.
+     * @param[in] localShip The ship to act on.
+     * @param[in] newXPosition The new X position to set on the camera window.
+     */
     void setDisplayXPosition(Ship* localShip, int newXPosition) {
         localShip->displayXPosition = newXPosition;
     }
 
+    /**
+     * Gets a ship's X position on the camera window.
+     * @param[in] localShip The ship to act on.
+     * @param[out] displayXPosition The X position of the ship on the camera window.
+     */
     int getDisplayXPosition(Ship* localShip) {
         return(localShip->displayXPosition);
     }
 
+    /**
+     * Sets a ship's Y position on the camera window.
+     * @param[in] localShip The ship to act on.
+     * @param[in] newYPosition The new Y position to set on the camera window.
+     */
     void setDisplayYPosition(Ship* localShip, int newYPosition) {
         localShip->displayYPosition = newYPosition;
     }
 
+    /**
+     * Gets a ship's Y position on the camera window.
+     * @param[in] localShip The ship to act on.
+     * @param[out] displayYPosition The Y position of the ship on the camera window.
+     */
     int getDisplayYPosition(Ship* localShip) {
         return(localShip->displayYPosition);
     }
 
+    /**
+     * Sets the display position of a ship based on where the camera window has visibility.
+     * @param[in] localShip The ship to act on.
+     * @param[in] screenYPos The Y position of the camera on the map.
+     * @param[in] screenXPos The X position of the camera on the map.
+     */
     void updateDisplayPosition(Ship* localShip, int screenYPos, int screenXPos) {
         int newX = getXPosition(localShip) - screenXPos;
         int newY = getYPosition(localShip) - screenYPos;
@@ -99,6 +170,12 @@
         setDisplayYPosition(localShip, newY);
     }
 
+    /**
+     * Sets a ship's direction of travel.
+     * Ranges between 0 and 2Pi radians.
+     * @param[in] localShip The ship to act on.
+     * @param[in] newDirection The new direction for the ship.
+     */
     void setDirection(Ship* localShip, double newDirection) {
         while (newDirection < 0) {
             newDirection += (2*M_PI);
@@ -110,26 +187,57 @@
         localShip->direction = newDirection;
     }
 
+    /**
+     * Gets a ship's direction of travel.
+     * Ranges between 0 and 2Pi radians.
+     * @param[in] localShip The ship to act on.
+     * @param[out] direction The direction of travel for the ship, in radians.
+     */
     double getDirection(Ship* localShip) {
         return(localShip->direction);
     }
 
+    /**
+     * Sets a ship's X velocity on the map.
+     * @param[in] localShip The ship to act on.
+     * @param[in] newVelocity The new X velocity to set for the ship.
+     */
     void setXVelocity(Ship* localShip, double newVelocity) {
         localShip->xVelocity = newVelocity;
     }
 
+    /**
+     * Gets a ship's X velocity on the map.
+     * @param[in] localShip The ship to act on.
+     * @param[out] xVelocity The X velocity of the ship.
+     */
     double getXVelocity(Ship* localShip) {
         return(localShip->xVelocity);
     }
 
+    /**
+     * Sets a ship's Y velocity on the map.
+     * @param[in] localShip The ship to act on.
+     * @param[in] newVelocity The new Y velocity to set for the ship.
+     */
     void setYVelocity(Ship* localShip, double newVelocity) {
         localShip->yVelocity = newVelocity;
     }
 
+    /**
+     * Gets a ship's Y velocity on the map.
+     * @param[in] localShip The ship to act on.
+     * @param[out] yVelocity The Y velocity of the ship.
+     */
     double getYVelocity(Ship* localShip) {
         return(localShip->yVelocity);
     }
 
+    /**
+     * Gets the absolute speed of a ship.
+     * @param[in] localShip The ship to act on.
+     * @param[out] speed The absolute speed of the ship.
+     */
     double getSpeed(Ship* localShip) {
         double xSpeed = fabs(getXVelocity(localShip));
         double ySpeed = fabs(getYVelocity(localShip));
@@ -138,44 +246,89 @@
         return speed;
     }
 
-    // Port = left turn
+    // Port = anticlockwise turn
+    /**
+     * Increases the Port side turn of a ship.
+     * This corresponds to an anticlockwise turn.
+     * @param[in] localShip The ship to act on.
+     */
     void incPortTurn(Ship* localShip) {
         if(localShip->turnLevel == -3)
             return;
         localShip->turnLevel--;
     }
 
-    // Starboard = right turn
+    // Starboard = clockwise turn
+    /**
+     * Increases the Starboard side turn of a ship.
+     * This corresponds to a clockwise turn.
+     * @param[in] localShip The ship to act on.
+     */
     void incStarboardTurn(Ship* localShip) {
         if(localShip->turnLevel == 3)
             return;
         localShip->turnLevel++;
     }
 
+    /**
+     * Gets the turn level of a ship.
+     * Ranges from -3 to 3.
+     * @param[in] localShip The ship to act on.
+     * @param[out] turnLevel The turn level of the ship.
+     */
     int getTurnLevel(Ship* localShip) {
         return(localShip->turnLevel);
     }
 
+    /**
+     * Sets a ship's firing rate.
+     * @param[in] localShip The ship to act on.
+     * @param[in] newFiringSpeed The new rate at which to fire.
+     */
     void setFiringSpeed(Ship* localShip, double newFiringSpeed) {
         localShip->firingSpeed = newFiringSpeed;
     }
 
+    /**
+     * Gets a ship's firing rate.
+     * @param[in] localShip The ship to act on.
+     * @param[out] firingSpeed The rate of fire for the ship.
+     */
     double getFiringSpeed(Ship* localShip) {
         return(localShip->firingSpeed);
     }
 
+    /**
+     * Toggles a ship's sails.
+     * @param[in] localShip The ship to act on.
+     */
     void switchSails(Ship* localShip) {
         localShip->sailsOpen = !(localShip->sailsOpen);
     }
 
+    /**
+     * Gets a ship's sail status.
+     * @param[in] localShip The ship to act on.
+     * @param[out] sailsOpen Whether the ship's sails are open.
+     */
     int getSailStat(Ship* localShip) {
         return(localShip->sailsOpen);
     }
 
+    /**
+     * Sets a ship's sprite file path.
+     * @param[in] localShip The ship to act on.
+     * @param[in] newPath The new sprite file path to set.
+     */
     void setSpritePath(Ship* localShip, char* newPath) {
         strcpy(localShip->spritePath, newPath);
     }
 
+    /**
+     * Sets a ship's sprite based on its direction of travel.
+     * @param[in] localShip The ship to act on.
+     * @param[in] orientation The orientation level of the ship.
+     */
     void setSpriteCompass(Ship* localShip, int orientation) {
         if (orientation < 0 || orientation > 7)
             return;
@@ -189,10 +342,19 @@
         setSpritePath(localShip, path);
     }
 
+    /**
+     * Gets a ship's sprite file path.
+     * @param[in] localShip The ship to act on.
+     * @param[out] spritePath The sprite file path of the ship.
+     */
     char* getSpritePath(Ship* localShip) {
         return localShip->spritePath;
     }
 
+    /**
+     * Updates a ship's sprite based on its direction of travel.
+     * @param[in] localShip The ship to act on.
+     */
     void updateSprite(Ship* localShip) {
         double orientation = getDirection(localShip);
 
@@ -216,6 +378,11 @@
             setSpriteCompass(localShip, SHIP_NORTH_WEST);
     }
 
+    /**
+     * Simulate the sailing movement of a ship in the wind.
+     * @param localShip The ship to act on.
+     * @param localWind The wind acting on the ship.
+     */
     void sail(Ship* localShip, Wind* localWind) {
         double shipSpeed = getSpeed(localShip);
         int turnLevel = getTurnLevel(localShip);
